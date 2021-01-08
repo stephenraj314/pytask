3cfbcb6baa0ae2f91ad083ce91f4fe530a284754
stephen <stephen@gma>
2021-01-08 23:43:57 +0530

    review1

--- guessnum.py
+++ guessnum.py
@@ -4,7 +4,8 @@ def getdata():
     return list( int(x) for x in input("Enter Your Guess for Four Digit Number : "))
 
 
-comp=list(ran.randint(0,9) for x in range(4))
+#comp=list(ran.randint(0,9) for x in range(4))
+comp=[4,1,3,2]
 print(comp)
 dup=comp.copy()
 user=getdata()
@@ -15,22 +16,28 @@ while loop:
     while i<4:
         if comp[i] == user[i]:
             flag[i]='C'
-            #del comp[i]          
-        elif user[i] in comp :
+            dup[i]='X'         
+        elif user[i] in dup :
             flag[i]='P'
         else:
             flag[i]='X'
         i+=1
+    if 'P' in flag: continue
     print(*user)    
     print(*flag)
     for x in flag:
-        if x=='C':
+        if all(ele == 'C' for ele in flag):
             loop=False
+            break
     user =getdata()
+    print(flag)
+    print(dup)
+    dup=comp.copy()
+    print(dup)
        
 print("congratulation You Found the Number ")             
-
-
+print(comp)
+print(user)
 
 
 
